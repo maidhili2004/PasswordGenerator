6ced3755dae6f5e7188c7ea9dd59fcca05d9c643
maidhili2004 <maidhilibathina3@gmail.com>
2025-07-02 10:35:50 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,63 +1,22 @@
 import random
+import string
 
-def generatePassword(pwlength):
+def generate_password(length):
+    if length < 8:
+        length = 8
 
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-    passwords = [] 
-
-    for i in pwlength:
-        
-        password = "" 
-        for j in range(i):
-            next_letter_index = random.randrange(len(alphabet))
-            password = password + alphabet[next_letter_index]
-        
-        password = replaceWithNumber(password)
-        password = replaceWithUppercaseLetter(password)
-        
-        passwords.append(password) 
-    
-    return passwords
-
-
-def replaceWithNumber(pword):
-    for i in range(random.randrange(1,3)):
-        replace_index = random.randrange(len(pword)//2)
-        pword = pword[0:replace_index] + str(random.randrange(10)) + pword[replace_index+1:]
-        return pword
-
-
-def replaceWithUppercaseLetter(pword):
-    for i in range(random.randrange(1,3)):
-        replace_index = random.randrange(len(pword)//2,len(pword))
-        pword = pword[0:replace_index] + pword[replace_index].upper() + pword[replace_index+1:]
-        return pword
-
-
+    characters = string.ascii_letters + string.digits + "!@#$%^&*()_+"
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
 
 def main():
-    
-    numPasswords = int(input("How many passwords do you want to generate? "))
-    
-    print("Generating " +str(numPasswords)+" passwords")
-    
-    passwordLengths = []
-
-    print("Minimum length of password should be 8")
-
-    for i in range(numPasswords):
-        length = int(input("Enter the length of Password #" + str(i+1) + " "))
-        if length<8:
-            length = 8
-        passwordLengths.append(length)
-    
-    
-    Password = generatePassword(passwordLengths)
-
-    for i in range(numPasswords):
-        print ("Password #"+str(i+1)+" = " + Password[i])
-
-
+    num = int(input("How many passwords do you want to generate? "))
+    with open("passwords.txt", "w") as file:
+        for i in range(num):
+            length = int(input(f"Enter length for Password #{i+1} (min 8): "))
+            password = generate_password(length)
+            print(f"Password #{i+1} = {password}")
+            file.write(f"Password #{i+1}: {password}\n")
+    print("✅ All passwords saved to 'passwords.txt'")
 
 main()
